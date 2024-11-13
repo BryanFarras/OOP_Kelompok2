49a9392d9f12010d820773de23f21369d5e1b121
BryanFarras <farrasbryan06@gmail.com>
2024-11-13 14:33:12 +0700

    testpush

---
+++ OOP_Kelompok2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Kelompok2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+
+        }
+    }
+}
