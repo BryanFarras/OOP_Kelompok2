689923ba11d2eae21d1d01c900036663077143c8
BryanFarras <farrasbryan06@gmail.com>
2024-11-17 19:06:19 +0700

    progress 3

--- OOP_Kelompok2/Program.cs
+++ OOP_Kelompok2/Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace OOP_Kelompok2
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             Awake();  
@@ -17,7 +19,13 @@ namespace OOP_Kelompok2
 
         static void Awake()
         {
+            
             Console.WriteLine("Game is initializing...");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Memunculkan form utama
+            Application.Run(new MainForm());
         }
 
         static void Start()
